b737b41a91719ff39825a328d547a92d3291b7bb
Yuktika089 <95289306+Yuktika089@users.noreply.github.com>
2022-05-26 15:23:01 +0530

    Update movierec_app.py

--- movierec_app.py
+++ movierec_app.py
@@ -32,6 +32,12 @@ if __name__ == '__main__':
               'Reality-TV', 'Romance', 'Sci-Fi', 'Short', 'Sport', 'Thriller', 'War', 'Western']
 
     movies = [title[0] for title in movie_titles]
+    name = st.text_input("Enter Your name", "Type Here ...")
+    if(st.button('Submit')):
+    result = name.title()
+    st.success(result)
+    
+    st.title("Recliner Lounge")
     st.header('Which Movie To Watch Today?')
     st.subheader('One solution for all movie cravings!')
     #loading image from system
